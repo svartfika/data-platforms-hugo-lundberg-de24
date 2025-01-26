20b147a47f20b2ee51842397ed97a2b1d5be543b
svartfika <svartfika@proton.me>
2025-01-26 16:42:23 +0100

    getting additional variables

--- extras/docker_env_test/src/config/config.py
+++ extras/docker_env_test/src/config/config.py
@@ -4,3 +4,5 @@ import os
 class Config:
     HOME = os.environ["HOME"]
     API_KEY = os.environ["API_KEY"]
+    BOOL_VAR = os.environ["BOOL_VAR"] == "true"
+    FROM_ENV_FILE = os.environ["FROM_ENV_FILE"]
--- extras/docker_env_test/src/main.py
+++ extras/docker_env_test/src/main.py
@@ -3,3 +3,5 @@ from config import config
 if __name__ == "__main__":
     print(f"{config.HOME=}")
     print(f"{config.API_KEY=}")
+    print(f"{config.BOOL_VAR=}")
+    print(f"{config.FROM_ENV_FILE=}")
