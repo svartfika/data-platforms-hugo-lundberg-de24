2a5c714dc08ac22ad79f76a33cf044d8217f569d
svartfika <svartfika@proton.me>
2025-01-26 01:28:20 +0100

    fix

--- exercises/exercise_2/src/ls_packages.py
+++ exercises/exercise_2/src/ls_packages.py
@@ -4,7 +4,6 @@ import sys
 print("Platform: ", sys.platform)
 print("Python version: ", sys.version)
 
-installed_dists = [dist for dist in md.distributions()]
 print("Installed Python distributions:")
-for dist in sorted(installed_dists, key=lambda x: x.name):
+for dist in sorted(md.distributions(), key=lambda x: x.name):
     print(dist.name, dist.version)
