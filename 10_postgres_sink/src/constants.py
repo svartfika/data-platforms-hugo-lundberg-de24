826c97f3ab59fc4b09dbff292b9ced26224de268
svartfika <svartfika@proton.me>
2025-02-07 19:22:11 +0100

    change: added constants

--- 10_postgres_sink/src/constants.py
+++ 10_postgres_sink/src/constants.py
@@ -8,4 +8,10 @@ dotenv.load_dotenv()
 KAFKA_HOST: Final[str] = str(os.getenv("KAFKA_HOST", "localhost"))
 KAFKA_PORT: Final[int] = int(os.getenv("KAFKA_PORT", 9092))
 
+POSTGRES_HOST: Final[str] = os.getenv("POSTGRES_HOST", "localhost")
+POSTGRES_PORT: Final[int] = int(os.getenv("POSTGRES_PORT", 5432))
+POSTGRES_USER: Final[str] = os.getenv("POSTGRES_USER", "postgres")
+POSTGRES_PASSWORD: Final[str] = os.getenv("POSTGRES_PASSWORD", "")
+POSTGRES_DB: Final[str] = os.getenv("POSTGRES_DB", "")
+
 COINMARKETCAP_API_KEY: Final[str] = os.getenv("COINMARKETCAP_API_KEY", "")
